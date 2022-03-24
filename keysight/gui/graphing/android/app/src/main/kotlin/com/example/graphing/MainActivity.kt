a03f58014ca00f9ee59dc7f62f68552a097a5753
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2022-03-24 11:33:35 -0400

    added graphing utilities

---
+++ keysight/gui/graphing/android/app/src/main/kotlin/com/example/graphing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.graphing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ keysight/gui/graphing/keysight_gui/android/app/src/main/kotlin/com/example/keysight_gui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.keysight_gui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
