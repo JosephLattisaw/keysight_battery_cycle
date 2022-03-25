4c6ab1b7ac59a16ddd4eefbb2f24902a75c3c81b
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2022-03-24 22:30:14 -0400

    completed instrument connection page

--- keysight/gui/graphing/keysight_gui/lib/tab_widget.dart
+++ keysight/gui/graphing/keysight_gui/lib/tab_widget.dart
@@ -55,8 +55,61 @@ class TabWidget extends HookWidget {
               ],
             ),
             child: TabBarView(
-              children: const [
-                Text("tab 1"),
+              children: [
+                ListView(
+                  children: [
+                    Container(
+                      color: Colors.white.withOpacity(0.1),
+                      child: ListTile(
+                        title: Text(
+                          "Keysight BT2203A Series Charge-Discharge System",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        leading: Image.asset(
+                          'assets/BT2202A.png',
+                          fit: BoxFit.contain,
+                          width: 70,
+                        ),
+                        trailing: ElevatedButton(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("Connect"),
+                          ),
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.green,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.white.withOpacity(0.2),
+                      child: ListTile(
+                        title: Text(
+                          "Keysight DAQ970A Data Acquisition System",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        leading: Image.asset(
+                          'assets/DAQ970A.png',
+                          fit: BoxFit.contain,
+                          width: 70,
+                        ),
+                        trailing: ElevatedButton(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("Connect"),
+                          ),
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.green,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
                 Text("tab 2"),
                 Text("tab 3"),
                 Text("tab 4"),
