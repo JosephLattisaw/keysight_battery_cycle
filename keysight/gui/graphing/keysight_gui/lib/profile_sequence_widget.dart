4984a409cd5de579defdfc64369cb3d94d9503ea
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2022-03-29 17:36:36 -0400

    finished making button placement of sequence tab

--- keysight/gui/graphing/keysight_gui/lib/profile_sequence_widget.dart
+++ keysight/gui/graphing/keysight_gui/lib/profile_sequence_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:keysight_gui/sequence_list_view.dart';
+import 'package:flutter_spinbox/material.dart';
+import 'package:flutter_spinbox/cupertino.dart';
 
 class ProfileSequenceWidget extends StatelessWidget {
   @override
@@ -16,12 +18,222 @@ class ProfileSequenceWidget extends StatelessWidget {
             ),
           ),
           Expanded(
-              flex: 3,
-              child: Container(
-                height: double.infinity,
-                color: Colors.white,
-                child: TextFormField(),
-              )),
+            flex: 3,
+            child: Container(
+              height: double.infinity,
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "Enter Name of Sequence",
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        Expanded(
+                          child: SpinBox(
+                            min: 0,
+                            max: 50,
+                            value: 15,
+                            spacing: 24,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              contentPadding: const EdgeInsets.only(
+                                left: 0,
+                                bottom: 20,
+                                right: 0,
+                                top: 20,
+                              ),
+                              hintText: "hint",
+                              labelText: "C Rating (A)",
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        Expanded(
+                          child: SpinBox(
+                            min: 0,
+                            max: 50,
+                            value: 15,
+                            spacing: 24,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              contentPadding: const EdgeInsets.only(
+                                left: 0,
+                                bottom: 20,
+                                right: 0,
+                                top: 20,
+                              ),
+                              hintText: "hint",
+                              labelText: "Over Voltage (V)",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "Enter Serial Number of Cell",
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        Expanded(
+                          child: SpinBox(
+                            min: 0,
+                            max: 50,
+                            value: 15,
+                            spacing: 24,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              contentPadding: const EdgeInsets.only(
+                                left: 0,
+                                bottom: 20,
+                                right: 0,
+                                top: 20,
+                              ),
+                              hintText: "hint",
+                              labelText: "Capacity (Ah)",
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        Expanded(
+                          child: SpinBox(
+                            min: 0,
+                            max: 50,
+                            value: 15,
+                            spacing: 24,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              contentPadding: const EdgeInsets.only(
+                                left: 0,
+                                bottom: 20,
+                                right: 0,
+                                top: 20,
+                              ),
+                              hintText: "hint",
+                              labelText: "Under Voltage (V)",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Include any additional comments here.",
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      "The sequence is empty. Use Create New Step to add a step.",
+                    ),
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          color: Colors.black,
+                          child: Column(
+                            children: [
+                              Text(
+                                "Create New",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  children: [
+                                    ElevatedButton(
+                                      child: Text("Step"),
+                                      onPressed: () {},
+                                    ),
+                                    SizedBox(width: 8),
+                                    ElevatedButton(
+                                      child: Text("Test"),
+                                      onPressed: () {},
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        Container(
+                          color: Colors.black,
+                          child: Column(
+                            children: [
+                              Text(
+                                "Selection",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  children: [
+                                    ElevatedButton(
+                                      child: Text("Step"),
+                                      onPressed: () {},
+                                    ),
+                                    SizedBox(width: 8),
+                                    ElevatedButton(
+                                      child: Text("Step"),
+                                      onPressed: () {},
+                                    ),
+                                    SizedBox(width: 8),
+                                    ElevatedButton(
+                                      child: Text("Step"),
+                                      onPressed: () {},
+                                    ),
+                                    SizedBox(width: 8),
+                                    ElevatedButton(
+                                      child: Text("Step"),
+                                      onPressed: () {},
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
