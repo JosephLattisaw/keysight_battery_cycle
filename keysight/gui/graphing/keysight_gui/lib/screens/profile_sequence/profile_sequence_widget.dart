62b63b4e180e593158b5c3038f65e6a3879f79ee
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2022-03-31 14:55:14 -0400

    can select the rows in the steps widget

--- keysight/gui/graphing/keysight_gui/lib/screens/profile_sequence/profile_sequence_widget.dart
+++ keysight/gui/graphing/keysight_gui/lib/screens/profile_sequence/profile_sequence_widget.dart
@@ -106,6 +106,7 @@ class ProfileSequenceWidget extends HookWidget {
 
     final count = useState(0);
     final editStepsView = useState(0);
+    final dataTableSelectedIndex = useState(-1);
 
     return Container(
       child: Row(
@@ -340,7 +341,110 @@ class ProfileSequenceWidget extends HookWidget {
                       ),
                       style: TextStyle(color: Colors.white),
                     ),
-                    Expanded(child: StepWidgetView(value: 0)),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    DataTable(
+                      headingRowColor: MaterialStateColor.resolveWith(
+                          (states) => Colors.blue.shade900),
+                      showCheckboxColumn: false,
+                      columns: [
+                        DataColumn(
+                          label: Text(
+                            'Function',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Step',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Action / Step Type',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Voltage (V)',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Current (A)',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Time',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                      rows: List<DataRow>.generate(
+                        5,
+                        (index) => DataRow(
+                          color: MaterialStateProperty.resolveWith<Color>(
+                              (states) {
+                            if (index == dataTableSelectedIndex.value)
+                              return Colors.blue;
+                            if (index % 2 == 0) return Colors.grey.shade700;
+                            return Colors.grey.shade800;
+                          }),
+                          onSelectChanged: (value) {
+                            if (value == false)
+                              dataTableSelectedIndex.value = -1;
+                            else
+                              dataTableSelectedIndex.value = index;
+                          },
+                          selected: index == dataTableSelectedIndex.value,
+                          cells: <DataCell>[
+                            DataCell(
+                              Text(
+                                'Step',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            DataCell(
+                              Text(
+                                '1',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            DataCell(
+                              Text(
+                                'Charge at',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            DataCell(
+                              Text(
+                                '4.0V',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            DataCell(
+                              Text(
+                                '20A',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            DataCell(
+                              Text(
+                                '1200s',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    //Expanded(child: StepWidgetView(value: 0)),
                     /*AnimatedSwitcher(
                       duration: const Duration(milliseconds: 500),
                       transitionBuilder: (child, animation) {
