28659f1fa06e55cbcdb3ec9589aaae6a1a39e60e
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2022-04-09 01:28:16 -0400

    added scroll view to measurements table

--- keysight/gui/graphing/keysight_gui/lib/screens/measurements/measurement_widget.dart
+++ keysight/gui/graphing/keysight_gui/lib/screens/measurements/measurement_widget.dart
@@ -77,80 +77,82 @@ class MeasurementWidget extends HookWidget {
               width: 12,
             ),
             Expanded(
-              child: DataTable(
-                headingRowColor: MaterialStateColor.resolveWith(
-                    (states) => Colors.blue.shade900),
-                columns: [
-                  DataColumn(
-                    label: Text(
-                      'Cell Number',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Voltage (V)',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Current (A)',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Capacity (aH)',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Status',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
-                rows: List<DataRow>.generate(
-                  32,
-                  (index) => DataRow(
-                    color: MaterialStateProperty.resolveWith<Color>((states) {
-                      if (index % 2 == 0) return Colors.grey.shade700;
-                      return Colors.grey.shade800;
-                    }),
-                    cells: <DataCell>[
-                      DataCell(
-                        Text(
-                          'Data',
-                          style: TextStyle(color: Colors.white),
-                        ),
+              child: SingleChildScrollView(
+                child: DataTable(
+                  headingRowColor: MaterialStateColor.resolveWith(
+                      (states) => Colors.blue.shade900),
+                  columns: [
+                    DataColumn(
+                      label: Text(
+                        'Cell Number',
+                        style: TextStyle(color: Colors.white),
                       ),
-                      DataCell(
-                        Text(
-                          'Data',
-                          style: TextStyle(color: Colors.white),
-                        ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Voltage (V)',
+                        style: TextStyle(color: Colors.white),
                       ),
-                      DataCell(
-                        Text(
-                          'Data',
-                          style: TextStyle(color: Colors.white),
-                        ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Current (A)',
+                        style: TextStyle(color: Colors.white),
                       ),
-                      DataCell(
-                        Text(
-                          'Data',
-                          style: TextStyle(color: Colors.white),
-                        ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Capacity (aH)',
+                        style: TextStyle(color: Colors.white),
                       ),
-                      DataCell(
-                        Text(
-                          'Data',
-                          style: TextStyle(color: Colors.white),
-                        ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        'Status',
+                        style: TextStyle(color: Colors.white),
                       ),
-                    ],
+                    ),
+                  ],
+                  rows: List<DataRow>.generate(
+                    32,
+                    (index) => DataRow(
+                      color: MaterialStateProperty.resolveWith<Color>((states) {
+                        if (index % 2 == 0) return Colors.grey.shade700;
+                        return Colors.grey.shade800;
+                      }),
+                      cells: <DataCell>[
+                        DataCell(
+                          Text(
+                            'Data',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        DataCell(
+                          Text(
+                            'Data',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        DataCell(
+                          Text(
+                            'Data',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        DataCell(
+                          Text(
+                            'Data',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        DataCell(
+                          Text(
+                            'Data',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
