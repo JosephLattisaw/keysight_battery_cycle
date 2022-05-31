99e73e7aa0cd18deae372bd55571052b6a3c9ee6
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2022-05-31 15:54:06 -0400

    moved keysight_gui to the old og directory

---
+++ keysight_gui.og/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:keysight_gui/home_page.dart';
+import 'package:keysight_gui/keysight_c_api.dart' as c_api;
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => c_api.KeysightCAPI(),
+          lazy: false,
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            scaffoldBackgroundColor:
+                Color(Colors.black.value).withOpacity(0.86)),
+        home: const HomePage(),
+      ),
+    );
+  }
+}
